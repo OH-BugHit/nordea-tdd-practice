deae8334981af17de88848ba8c48d1c3b3fe85d5
Olli Hilke <hilkolli@student.uef.fi>
2025-03-25 10:59:20 +0200

    returns array with one element when one given name

--- src/example/example.spec.ts
+++ src/example/example.spec.ts
@@ -13,10 +13,17 @@ Sort array of String
 describe('Sort array of Strings', () => {
     it('returs empty array when no names are given', () => {
         const result = sortArray();
+
         expect(result).toEqual([]);
+    });
+
+    it('returns array with one element when one given name', () => {
+        const result = sortArray('Tomi');
+
+        expect(result).toEqual(['Tomi']);
     });
 });
 
-function sortArray(): string[] {
-    return [];
+function sortArray(...names: string[]): string[] {
+    return names;
 }
