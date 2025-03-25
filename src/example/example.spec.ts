3d6e6eeb75970c11210ca9de9fa1ce62ba2dbd63
Olli Hilke <hilkolli@student.uef.fi>
2025-03-25 12:40:54 +0200

    returns array of names sorted by small alphabetic first

--- src/example/example.spec.ts
+++ src/example/example.spec.ts
@@ -34,13 +34,17 @@ describe('Sort array of Strings', () => {
         expect(result).toEqual(['Aapo', 'Petteri']);
     });
 
-    it('returns array of names sorted by capitalization', () => {
-        const result = sortArray('tomi', 'Tomi');
+    it('returns array of names sorted by small alphabetic first', () => {
+        const result = sortArray('Tomi', 'tomi');
 
-        expect(result).toEqual(['Tomi', 'tomi']);
+        expect(result).toEqual(['tomi', 'Tomi']);
     });
 });
 
 function sortArray(...names: string[]): string[] {
-    return names.filter((name) => !!name).sort();
+    const removeEmptyNames = (name: string) => !!name;
+    const sortByLowerCase = (a: string, b: string) =>
+        a.toLowerCase() > b.toLowerCase() ? 1 : -1;
+
+    return names.filter(removeEmptyNames).sort().sort(sortByLowerCase);
 }
